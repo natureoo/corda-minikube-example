d227acc8a0b688af642f604b251b83c479fb7a6f
natureoo <924943578@qq.com>
2023-03-03 00:32:20 +0800

    fix

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -6,4 +6,4 @@ set -eux
 #  exec java -jar corda.jar --config-file=${CONFIG_FILE} --no-local-shell --log-to-console
 #fi
 
-exec java -jar corda-webserver.jar --config-file=${CONFIG_FILE} --log-to-console
+#exec java -jar corda-webserver.jar --config-file=${CONFIG_FILE} --log-to-console
