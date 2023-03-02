d2f9f142876f575b8128152235edbd3967d1f83c
natureoo <924943578@qq.com>
2023-03-03 00:20:30 +0800

    fix

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -2,8 +2,8 @@
 
 set -eux
 
-if [ -z "${1-}" ]; then
-  exec java -jar corda.jar --config-file=${CONFIG_FILE} --no-local-shell --log-to-console
-fi
+#if [ -z "${1-}" ]; then
+#  exec java -jar corda.jar --config-file=${CONFIG_FILE} --no-local-shell --log-to-console
+#fi
 
 exec java -jar corda-webserver.jar --config-file=${CONFIG_FILE} --log-to-console
