9325f402628ed839a89ea77571cde35bee3a394d
natureoo <924943578@qq.com>
2023-03-06 11:34:18 +0800

    fix

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -13,6 +13,6 @@ cd certificates
 curl http://host.minikube.internal:8080/network-map/truststore -o ./network-truststore.jks
 cd /app
 echo "$PWD"
-java -jar corda.jar --config-file=${CONFIG_FILE} --initial-registration --base-directory /app --network-root-truststore ./certificates/network-truststore.jks --network-root-truststore-password trustpass
 java -jar corda.jar run-migration-scripts --config-file=${CONFIG_FILE} --core-schemas --app-schemas --allow-hibernate-to-manage-app-schema
+java -jar corda.jar --config-file=${CONFIG_FILE} --initial-registration --base-directory /app --network-root-truststore ./certificates/network-truststore.jks --network-root-truststore-password trustpass
 java -jar corda.jar --config-file=${CONFIG_FILE}
