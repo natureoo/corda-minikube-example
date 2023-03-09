49e8f3eb272a99916952e2c4eb791a0009cfff6c
natureoo <924943578@qq.com>
2023-03-09 19:45:23 +0800

    fix

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -19,12 +19,12 @@ mv bak/*.jar cordapps/
 java -jar corda.jar run-migration-scripts --config-file=${CONFIG_FILE} --core-schemas --app-schemas --allow-hibernate-to-manage-app-schema
 
 echo $ROLE
-if [[ ! -z "${ROLE}" &&  "NOTARY" = ${ROLE} ]]; then
-    TOKEN=`curl -X POST "http://host.minikube.internal:8080/admin/api/login" -H  "accept: text/plain" -H  "Content-Type: application/json" -d "{  \"user\": \"sa\",  \"password\": \"admin\"}"`
-    NODEINFO=`ls nodeInfo*`
-    curl -X POST -H "Authorization: Bearer $TOKEN" -H "accept: text/plain" -H "Content-Type: application/octet-stream" --data-binary @$NODEINFO http://host.minikube.internal:8080/admin/api/notaries/validating
-    curl http://host.minikube.internal:8080/admin/api/notaries
-fi
+#if [[ ! -z "${ROLE}" &&  "NOTARY" = ${ROLE} ]]; then
+#    TOKEN=`curl -X POST "http://host.minikube.internal:8080/admin/api/login" -H  "accept: text/plain" -H  "Content-Type: application/json" -d "{  \"user\": \"sa\",  \"password\": \"admin\"}"`
+#    NODEINFO=`ls nodeInfo*`
+#    curl -X POST -H "Authorization: Bearer $TOKEN" -H "accept: text/plain" -H "Content-Type: application/octet-stream" --data-binary @$NODEINFO http://host.minikube.internal:8080/admin/api/notaries/validating
+#    curl http://host.minikube.internal:8080/admin/api/notaries
+#fi
 
 java -jar corda.jar --config-file=${CONFIG_FILE}
 
