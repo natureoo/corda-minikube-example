ae98d0208ebb9eea114475b6b6cc3a171d6feef6
natureoo <924943578@qq.com>
2023-03-09 19:07:07 +0800

    fix

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -19,7 +19,7 @@ mv bak/*.jar cordapps/
 java -jar corda.jar run-migration-scripts --config-file=${CONFIG_FILE} --core-schemas --app-schemas --allow-hibernate-to-manage-app-schema
 
 echo $ROLE
-if [ "NOTARY" = ${ROLE} ]; then
+if [ ! -z "${ROLE}" &&  "NOTARY" = ${ROLE} ]; then
     TOKEN=`curl -X POST "http://host.minikube.internal:8080/admin/api/login" -H  "accept: text/plain" -H  "Content-Type: application/json" -d "{  \"user\": \"sa\",  \"password\": \"admin\"}"`
     NODEINFO=`ls nodeInfo*`
     curl -X POST -H "Authorization: Bearer $TOKEN" -H "accept: text/plain" -H "Content-Type: application/octet-stream" --data-binary @$NODEINFO http://host.minikube.internal:8080/admin/api/notaries/validating
