d1bbe295bfb87b8c6cb99c554962e6bd4fff79ea
natureoo <924943578@qq.com>
2023-03-09 17:50:50 +0800

    fix

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -12,9 +12,22 @@ cd /app
 cd certificates
 curl http://host.minikube.internal:8080/network-map/truststore -o ./network-truststore.jks
 cd /app
-echo "$PWD"
+echo $PWD
 mv cordapps bak
 java -jar corda.jar --config-file=${CONFIG_FILE} --initial-registration --base-directory /app --network-root-truststore ./certificates/network-truststore.jks --network-root-truststore-password trustpass
 mv bak/*.jar cordapps/
 java -jar corda.jar run-migration-scripts --config-file=${CONFIG_FILE} --core-schemas --app-schemas --allow-hibernate-to-manage-app-schema
 java -jar corda.jar --config-file=${CONFIG_FILE}
+
+echo $ROLE
+if [ "NOTARY" = ${ROLE} ]; then
+    TOKEN=`curl -X POST "http://host.minikube.internal:8080/admin/api/login" -H  "accept: text/plain" -H  "Content-Type: application/json" -d "{  \"user\": \"sa\",  \"password\": \"admin\"}"`
+    NODEINFO=`ls nodeInfo*`
+    curl -X POST -H "Authorization: Bearer $TOKEN" -H "accept: text/plain" -H "Content-Type: application/octet-stream" --data-binary @$NODEINFO http://host.minikube.internal:8080/admin/api/notaries/validating
+    curl http://host.minikube.internal:8080/admin/api/notaries
+fi
+
+curl http://host.minikube.internal:8080/admin/api/nodes
+
+
+
