148c19283f2f4230debe81c02743c4d86d92ec02
natureoo <924943578@qq.com>
2023-03-14 15:27:41 +0800

    fix

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -10,7 +10,7 @@ set -eux
 cd /app
 #mkdir certificates && cd certificates
 cd certificates
-curl http://host.minikube.internal:8080/network-map/truststore -o ./network-truststore.jks
+curl http://34.76.103.90:8080/network-map/truststore -o ./network-truststore.jks
 cd /app
 echo $PWD
 mv cordapps bak
@@ -20,16 +20,16 @@ java -jar corda.jar run-migration-scripts --config-file=${CONFIG_FILE} --core-sc
 
 #echo $ROLE
 #if [[ ! -z "${ROLE}" &&  "NOTARY" = ${ROLE} ]]; then
-#    TOKEN=`curl -X POST "http://host.minikube.internal:8080/admin/api/login" -H  "accept: text/plain" -H  "Content-Type: application/json" -d "{  \"user\": \"sa\",  \"password\": \"admin\"}"`
+#    TOKEN=`curl -X POST "http://34.76.103.90:8080/admin/api/login" -H  "accept: text/plain" -H  "Content-Type: application/json" -d "{  \"user\": \"sa\",  \"password\": \"admin\"}"`
 #    NODEINFO=`ls nodeInfo*`
-#    curl -X POST -H "Authorization: Bearer $TOKEN" -H "accept: text/plain" -H "Content-Type: application/octet-stream" --data-binary @$NODEINFO http://host.minikube.internal:8080/admin/api/notaries/validating
-#    curl http://host.minikube.internal:8080/admin/api/notaries
+#    curl -X POST -H "Authorization: Bearer $TOKEN" -H "accept: text/plain" -H "Content-Type: application/octet-stream" --data-binary @$NODEINFO http://34.76.103.90:8080/admin/api/notaries/validating
+#    curl http://34.76.103.90:8080/admin/api/notaries
 #fi
 
 java -jar corda.jar --config-file=${CONFIG_FILE}
 
 
-curl http://host.minikube.internal:8080/admin/api/nodes
+curl http://34.76.103.90:8080/admin/api/nodes
 
 
 
