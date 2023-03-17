7a765c41bcdbdf2bd80680055ad84272f3b542c1
natureoo <924943578@qq.com>
2023-03-17 15:50:19 +0800

    fix

--- build-glcm/entrypoint.sh
+++ build-glcm/entrypoint.sh
@@ -2,14 +2,16 @@
 
 
 cd /app
+echo $PWD
 FILE=certificates
-if [ ! -f "$FILE" ]; then
+if [ ! -d "$FILE" ]; then
     echo "$FILE does not exist."
     mkdir certificates && cd certificates
     curl http://34.76.103.90:8080/network-map/truststore -o ./network-truststore.jks
     cd /app
     echo $PWD
     mv cordapps bak
+    echo $PWD
     java -jar corda.jar --config-file=${CONFIG_FILE} --initial-registration --base-directory /app --network-root-truststore ./certificates/network-truststore.jks --network-root-truststore-password trustpass
     mv bak/*.jar cordapps/
     java -jar corda.jar run-migration-scripts --config-file=${CONFIG_FILE} --core-schemas --app-schemas --allow-hibernate-to-manage-app-schema
--- build.sh
+++ build.sh
@@ -2,3 +2,8 @@ cd build
 docker build -t europe-west2-docker.pkg.dev/canvas-hook-339503/corda/corda:4.10 .
 cd build-glcm
 docker build -t europe-west2-docker.pkg.dev/canvas-hook-339503/corda/corda-glcm:4.10 .
+
+gcloud auth configure-docker europe-west2-docker.pkg.dev
+
+docker push europe-west2-docker.pkg.dev/canvas-hook-339503/corda/corda:4.10
+docker push europe-west2-docker.pkg.dev/canvas-hook-339503/corda/corda-glcm:4.10
