9493f941ffcd3b1588516a9072298c5094b79c92
natureoo <924943578@qq.com>
2023-03-20 14:55:29 +0800

    fix

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@ docker build -t europe-west2-docker.pkg.dev/canvas-hook-339503/corda/corda:4.9.6
 cd ../build-glcm
 docker build -t europe-west2-docker.pkg.dev/canvas-hook-339503/corda/corda-os:4.9.6 .
 
+cd ../build-cap
+docker build -t corda-poc:4.9.6 .
+
 gcloud auth configure-docker europe-west2-docker.pkg.dev
 
 docker push europe-west2-docker.pkg.dev/canvas-hook-339503/corda/corda:4.9.6
